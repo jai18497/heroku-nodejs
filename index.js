16fc23c22bbce832f79fcce6639e0b16fac626b2
root <root@LaptopAIT008.localdomain>
2021-01-29 14:03:17 +0530

    added database configurations

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+//Mongoose Settings
+//-----------------------------------------
+const {User} = require("./models");
+const mongoose = require("mongoose");
+
+console.log("mongoose stuff intialized");
+
+app.use((req, res, next) => {
+  console.log("use for mongoose callback");
+  if (mongoose.connection.readyState) {
+    console.log("if (mongoose.connection.readyState)");
+    next();
+  } else {
+    console.log("else (mongoose.connection.readyState)");
+    require("./mongo")().then(() => next());
+    console.log("else (mongoose.connection.readyState)");
+  }
+});
+
